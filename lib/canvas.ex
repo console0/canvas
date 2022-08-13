50f32ce9bc5b165fcd4662ba676f2e09d11bc2f8
Marc Slagle <console0@gmail.com>
2022-08-13 16:04:39 -0400

    Update canvas.ex

--- lib/canvas.ex
+++ lib/canvas.ex
@@ -21,13 +21,18 @@ defmodule Canvas do
     :debug
   end
 
-  def get_secret(_app,key) do
+  def load_secret(_app,key) do
+    # would be the for-real one where we get a val and return tuple
     {:error, "Secret not found: " <> key}
   end
 
+  def get_secret(app,key) do
+    load_secret(app,key) |> elem(1)
+  end
+
   def get_secret!(app,key) do
-    case get_secret(app,key) do
-      {:ok, result} -> result
+    case load_secret(app,key) do
+      {:ok, secret} -> secret
       {:error, message} -> raise RuntimeError, message: message
     end
   end
